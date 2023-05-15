39f0b1186f69079425a5b90b524a676a7ae6dd96
garimagautam01 <garima2131096@akgec.ac.in>
2023-05-16 00:32:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Gallery from "./Gallery";
+
+
+const App=()=>{
+  return(
+    <>
+       <h1 style={{textAlign: "center"}}>Image Gallery</h1>
+       <Gallery/>
+    </>
+  );
+}
+
+export default App;
